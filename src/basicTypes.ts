9910379e0bb1bb908c28fb1b43830cc3c60b7ed7
Iqbal Fasri <iqbalx64@gmail.com>
2021-01-05 22:46:52 +0700

    basic types

---
+++ src/basicTypes.ts
@@ -0,0 +1,9 @@
+// Boolean
+let isDone: boolean = true;
+
+// Number
+let decimal: number = 4;
+let hex: number = 0xf00d;
+let binary: number = 0b01010;
+let octal: number = 0o744;
+let big: bigint = 100n;
